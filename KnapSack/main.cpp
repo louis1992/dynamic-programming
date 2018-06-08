55741ee67dcb9dbd49f18cea3eb8dcaf23013f12
louis1992 <shinyk92@gmail.com>
2018-06-09 07:28:29 +0900

    Add lodCutting.

---
+++ KnapSack/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string tmp = "11";
+    cout << stoi(tmp) << endl;
+    return 0;    
+}
--- lodCutting/main.cpp
+++ lodCutting/main.cpp
@@ -7,30 +7,48 @@ using namespace std;
 
 void loadData(vector<int>& length, vector<int>& profit);
 int getMax(int x, int y);
-void cutRod(int length, int profit);
+int cutRod(int length, vector<int> profit);
 void print(vector<int> data);
 
 int main(int argc, char** argv) {
-    //vector<int> length;
-    //vector<int> profit;
-    //loadData(length, profit);
-    int length[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int profit[11] = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-    //print(length);
+    vector<int> length;
+    vector<int> profit;
+    loadData(length, profit);
     return 0;
 }
 
 void loadData(vector<int>& length, vector<int>& profit) {
     // TODO.
+    ifstream inFile("data.txt");
+    string buf;
+    string data_type;
+    while (inFile >> buf) {
+        if (buf == "length")
+            data_type = "length";
+        else if (buf == "profit")
+            data_type = "profit";
+        if (data_type == "length") {
+            length.push_back(std::stoi(buf));
+        }
+        else if (data_type == "profit") {
+            profit.push_back(std::stoi(buf));
+        }
+    }
+    inFile.close();
 }
 
 int getMax(int x, int y) {
     return x > y ? x : y;
 }
 
-void cutRod(int length, int profit) {
-    int maxProfit;
-
+int cutRod(int length, vector<int>& profit) {
+    int maxProfit = 0;
+    if (length <= 0)
+        return 0;
+    
+    for (int i = 0; i < length; ++i)
+        maxProfit = getMax(maxProfit, profit[i] + cutRod(length - i - 1, profit));
+    return maxProfit;
 }
 
 void print(vector<string> data) {
