e0c1c8590f92fef55ed582f2afba26d048ed52bd
louis1992 <shinyk92@gmail.com>
2018-06-09 05:40:53 +0900

    Add print LCS.

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -13,8 +13,9 @@ void print(vector<string> data);
 int main(int argc, char** argv) {
     vector<string> data;
     loadData(data);
-    LCS(data);
     print(data);
+    LCS(data);
+    
     return 0;
 }
 
@@ -34,22 +35,39 @@ void LCS(vector<string>& data) {
     unsigned int x = data[0].size();
     unsigned int y = data[0].size();
     int result[x+1][y+1];
-
+    
     for (int i = 0; i <= x; ++i) {
         for (int j = 0; j <=y; ++j) {
             if (i == 0 || j == 0)
                 result[i][j] = 0;
-            else if (data[0][i-1] == data[1][j-1])
+            else if (data[0][i-1] == data[1][j-1]) 
                 result[i][j] = result[i-1][j-1] + 1;
-            else
+            else 
                 result[i][j] = getMax(result[i-1][j], result[i][j-1]);
         }
     }
-
-    cout << result[x][y] <<endl;
+    
+    // Print LCS
+    int index = result[x][y];
+    int col = x, row = y;
+    char lcs[index];
+    while (col > 0 && row > 0) {
+        if (data[0][col-1] == data[1][row-1]) {
+            lcs[index-1] = data[0][col-1];
+            --col, --row, --index;
+        }
+        else if (result[col-1][row] > result[col][row-1])
+            --col;
+        else
+            --row;
+    }
+    cout << "// LCS : "<< lcs << endl;
+    cout << "// Length : " << result[x][y] <<endl;
+    cout << "////////////////////////////////"  << endl;
 }
 
 void print(vector<string> data) {
+    cout << "////////////////////////////////"  << endl;
     for (int i = 0; i !=data.size(); ++i)
-        cout << data[i] << endl;
+        cout << "// data "<< i << " : " << data[i] << endl;
 }
